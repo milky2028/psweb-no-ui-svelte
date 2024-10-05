6abf15a92a2d5398181a418190f44d746c97af43
Tyler Gross <tylergross28@gmail.com>
2024-10-05 08:01:22 -0400

    more description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,16 +4,28 @@
 	import Divider from '$lib/Divider.svelte';
 
 	let status = 'uninitialized';
-	let isolated = '...';
+	let isolated = false;
 
 	onMount(() => {
-		isolated = `${crossOriginIsolated}`;
+		isolated = crossOriginIsolated;
 	});
+
+	function onClear() {}
 </script>
 
 <Divider />
 
 <div><strong>Status:</strong> {status}</div>
 <div><strong>crossOriginIsolated:</strong> {isolated}</div>
+
+<Divider />
+
+<div>
+	This application allows you to step through Photoshop Web's booting process with a pre-configured
+	memory amount.
+	<br />
+	Use the checkbox next to each step to configure that step to run automatically on page reload.
+</div>
+<button on:click={onClear}>Clear Autoboot</button>
 
 <Divider />
